cb577bb581dcc01ed5057132e0b0282ae94bc433
dev0062 <zaindevalldino@gmail.com>
2026-04-14 11:29:29 +0700

    Mengatur tampilan awal form

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -265,5 +265,20 @@ namespace CRUDMahasiswaADO
             txtNIM.Focus();
         }
 
-        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbjk.Items.Clear();
+            cmbjk.Items.Add("L");
+            cmbjk.Items.Add("P");
+
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+            dataGridView1.ReadOnly = true;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            dataGridView1 .CellClick +=  dataGridView1_CellContentClick;
+
+        }
+    }
 }
