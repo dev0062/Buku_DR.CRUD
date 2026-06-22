60ba2a93df75ba03287305881bc664dc73420583
dev0062 <zaindevalldino@gmail.com>
2026-06-23 02:38:05 +0700

    Memebuat LogJika terjadi insert data

--- CRUDMahasiswaADO/DAL.cs
+++ CRUDMahasiswaADO/DAL.cs
@@ -210,6 +210,21 @@ namespace CRUDMahasiswaADO
 
             return dtMahasiswa;
         }
+
+        public void InsertLog(string message)
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand("sp_LogMessage", conn);
+
+            cmd.Parameters.AddWithValue("psn", message);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.ExecuteNonQuery();
+        }
+
     }
 
 }
